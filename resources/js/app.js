b8e6480ce7e41a2131c23c49ddcfe8bae11e407c
amardjidan <amardjidan@gmail.com>
2025-03-08 22:40:38 +0700

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import 'bootstrap';
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.toggle-sidebar-btn')?.addEventListener('click', function() {
+        document.body.classList.toggle('toggle-sidebar');
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Backend\PendidikanController;
+use App\Http\Controllers\Backend\PengalamanKerjaController;
 
-Route::resource('/dashboard',DashboardController::class);
+Route::resource('/dashboard', DashboardController::class);
+
+Route::prefix('backend')->group(function () {
+ Route::resource('dashboard', DashboardController::class);
+ Route::resource('pendidikan', PendidikanController::class);
+ Route::resource('pengalaman_kerja', PengalamanKerjaController::class);
+});
